e3f6c85547f52cbd1c27c17c92b5797f763ce5a0
Aidin Rasti <aidin.rasti@gmail.com>
2021-09-02 00:59:04 -0400

    fix export

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ export * from './core/Party.js'
 export * from './core/Role.js'
 export * from './core/Predicates.js'
 export * from './Utils.js'
+export * from './Events.js'
 
 // TODO update from java code
