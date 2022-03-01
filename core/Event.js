27d80d9d8c2ef8e3e0f042e4e1dab74b1159d1c2
Aidin Rasti <aidin.rasti@gmail.com>
2022-02-28 20:11:13 -0500

    change happen method

--- core/Event.js
+++ core/Event.js
@@ -6,7 +6,10 @@ class Event {
 
   happen(event) {
     this._triggered = true;
-    this._timestamp = new Date().toISOString();
+    const d = new Date();
+    d.setSeconds(0);
+    d.setMilliseconds(0);
+    this._timestamp = d.toISOString();
     if (event != null) {
       for (const key of Object.keys(event)) {
         this[key] = event[key];
