4a9a7812df117b8fd5ca7d7ff3232b21cc1a2489
Aidin Rasti <aidin.rasti@gmail.com>
2022-03-03 19:31:50 -0500

    update happensWithinSitutaion method

--- core/Predicates.js
+++ core/Predicates.js
@@ -19,7 +19,56 @@ const Predicates = {
   },
 
   happensWithinSituation(e, object, state) {
-    return e.hasHappened() && object.state === state; // TODO should check using methods
+    switch (state) {
+      case 'Power.Create':
+        return e.hasHappened() && object.isCreate();
+      case 'Power.UnsuccessfulTermination':
+        return e.hasHappened() && object.isUnsuccessfulTermination();
+      case 'Power.Active':
+        return e.hasHappened() && object.isActive();
+      case 'Power.InEffect':
+        return e.hasHappened() && object.isInEffect();
+      case 'Power.Suspension':
+        return e.hasHappened() && object.isSuspended();
+      case 'Power.SuccessfulTermination':
+        return e.hasHappened() && object.isSuccessfulTermination();
+
+      case 'Obligation.Create':
+        return e.hasHappened() && object.isCreated();
+      case 'Obligation.Discharge':
+        return e.hasHappened() && object.isDischarged();
+      case 'Obligation.Active':
+        return e.hasHappened() && object.isActive();
+      case 'Obligation.InEffect':
+        return e.hasHappened() && object.isInEffect();
+      case 'Obligation.Suspension':
+        return e.hasHappened() && object.isSuspended();
+      case 'Obligation.Violation':
+        return e.hasHappened() && object.isViolated();
+      case 'Obligation.Fulfillment':
+        return e.hasHappened() && object.isFulfilled();
+      case 'Obligation.UnsuccessfulTermination':
+        return e.hasHappened() && object.isUnsuccessfulTermination();
+
+      case 'Contract.Form':
+        return e.hasHappened() && object.isForm();
+      case 'Contract.UnAssign':
+        return e.hasHappened() && object.isUnassign();
+      case 'Contract.InEffect':
+        return e.hasHappened() && object.isInEffect();
+      case 'Contract.Suspension':
+        return e.hasHappened() && object.isSuspended();
+      case 'Contract.Rescission':
+        return e.hasHappened() && object.isRescission();
+      case 'Contract.SuccessfulTermination':
+        return e.hasHappened() && object.isSuccessfulTermination();
+      case 'Contract.UnsuccessfulTermination':
+        return e.hasHappened() && object.isUnsuccessfulTermination();
+      case 'Contract.Active':
+        return e.hasHappened() && object.isActive();
+      default:
+        return false;
+    }
   },
 
   happensWithinDate(e, start, end) {
